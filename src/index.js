da6abfae6bcffe59df6123ddb31db4347f9574ff
emiroquai <22440776+emiroquai@users.noreply.github.com.>
2023-11-22 18:58:59 +0100

    Added show/hide add project button func

--- src/index.js
+++ src/index.js
@@ -18,3 +18,20 @@ const TodoManager = (() => {
         Todo
     }
 })();
+
+//Screen Controller
+function ScreenController () {
+    const addListBtnDisp = document.getElementById('btn-add-list-disp')
+    const addListForm = document.getElementById('form-add-list') 
+    
+    addListBtnDisp.addEventListener('click', function() {
+        // Toggle the form's visibility
+    if (addListForm.style.display === "none" || addListForm.style.display === "") {
+        addListForm.style.display = "flex"; // Show the form
+    } else {
+        addListForm.style.display = "none"; // Hide the form
+    }
+    });
+};
+
+ScreenController();
