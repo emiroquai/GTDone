080ae3c9fc2a118a838a180486fa8c16a31c95fc
emiroquai <22440776+emiroquai@users.noreply.github.com.>
2023-11-24 19:22:09 +0100

    Switching lists logic added

--- src/index.js
+++ src/index.js
@@ -17,12 +17,6 @@ const TodoManager = (() => {
         id: 'Inbox',
         title: 'Inbox',
         tasks: [{
-            title: 'Do Laundry',
-            description: "Don't be lazy",
-            dueDate: new Date(2023, 10, 23),
-            priority: 'normal',
-            isDone: false
-        }, {
             title: 'Start coding the todo app',
             description: "Don't be lazy",
             dueDate: new Date(2023, 10, 23),
@@ -39,24 +33,51 @@ const TodoManager = (() => {
     }, {
         id: 'Today',
         title: 'Today',
-        tasks: []
+        tasks: [{
+            title: 'Do Laundry',
+            description: "Don't be lazy",
+            dueDate: new Date(2023, 10, 23),
+            priority: 'normal',
+            isDone: false
+        },],
+        isSelected: false
     } ,{
         id: 'Upcoming',
         title: 'Upcoming',
-        tasks: []
+        tasks: [],
+        isSelected: false
     } ,
         // User Lists
     {
-        id: 1,
+        id: 'Home',
         title: 'Home',
-        tasks: []
+        tasks: [{
+            title: 'Do Laundry',
+            description: "Don't be lazy",
+            dueDate: new Date(2023, 10, 23),
+            priority: 'normal',
+            isDone: false
+        }],
+        isSelected: false
     } , {
-        id: 2,
+        id: 'Work',
         title: 'Work',
-        tasks: []
+        tasks: [],
+        isSelected: false
     }]
     
     const getLists = () => lists;
+
+    let selectedList = lists[0];
+
+    const getSelectedList = () => selectedList;
+
+    function selectList(listId) {
+        selectedList.isSelected = false
+        const newList = lists.find(list => list.id === listId);
+        newList.isSelected = true;
+        selectedList = newList;
+    }
 
     // List factory
     function List(title) {
@@ -68,13 +89,15 @@ const TodoManager = (() => {
 
     return {
         getLists,
+        getSelectedList,
+        selectList,
         Todo,
         List,
     }
 })();
 
 //Screen Controller
-function ScreenController () {
+function ScreenController() {
     const addListBtnDisp = document.getElementById('btn-add-list-disp')
     const addListForm = document.getElementById('form-add-list') 
     const addListFormInput = document.getElementById('new-list-title')
@@ -91,47 +114,63 @@ function ScreenController () {
             form.style.display = "none"; // Hide the form
         }
     }
-
+    
     function rotateBtn(button) {
-         // Rotate button
-         button.classList.toggle("btn-rotated");
+        // Rotate button
+        button.classList.toggle("btn-rotated");
     }
-
-    addListBtnDisp.addEventListener('click', function() {
-        rotateBtn(addListBtnDisp);
-        toggleForm(addListForm);
-    });
-
-    addTaskBtnDisp.addEventListener('click', function() {
-        rotateBtn(addTaskBtnDisp);
-        toggleForm(addTaskForm);
-    });
-
+    
     function clearElement(element) {
         element.innerHTML = "";
     }
-
+    
     function render() {
-        clearElement(listsList);
-        lists.forEach(list => {
-           
+        
 
+        clearElement(listsList);
+
+        lists.forEach(list => {  
+            // Render user lists on the sidebar
+            // Skip the base lists
+            if (list.id === 'Inbox' || list.id === 'Today' || list.id === 'Upcoming') {
+                return
+            } else {
+            const listElement = document.createElement('li')
+            listElement.classList.add('list-name')
+            listElement.setAttribute('data-list-id', list.id)
+            
+            const iconElement = document.createElement("i");
+            iconElement.className = "fa-regular fa-folder";
+            iconElement.style.color = "#0061ff";
+            listElement.appendChild(iconElement)
+            
+            const textNode = document.createTextNode(list.title)
+            listElement.appendChild(textNode)
+            
+            listsList.appendChild(listElement)
+            }
+        });
+            
+        lists.forEach(list => {  
             // Render selected list  
-            const taskWrapper = document.getElementById('task-list-field')
             const selectedListTitle = document.getElementById('selected-list-title')
             const taskList = document.getElementById('task-list')
-            if(list.isSelected == true) {
-                
-                
+            let listTitles = document.querySelectorAll('li.list-name')
+
+            if (list.isSelected == true) {
+                // highlight selected list
+                const selectedListTitleSidebar = Array.from(listTitles).find(title => title.dataset.listId === list.id);
+                selectedListTitleSidebar.classList.add('selected');
+            
                 clearElement(selectedListTitle);
                 clearElement(taskList);
                 selectedListTitle.classList.add('list-name')
                 selectedListTitle.innerHTML = list.title
-
+                // Render tasks
                 list.tasks.forEach(task => {
                     const taskElement = document.createElement('li')
                     taskElement.classList.add('task')
-
+                    
                     const iconElement = document.createElement('i')
                     if (task.isDone == true) {
                         iconElement.className = "fa-solid fa-square-check red"
@@ -144,51 +183,62 @@ function ScreenController () {
                         }
                     }
                     taskElement.appendChild(iconElement)
-
+                    
                     const textNode = document.createTextNode(task.title)
                     taskElement.appendChild(textNode)
-
+                    
                     taskList.appendChild(taskElement)
                 })
             }
-
             
-            // Render user lists on the sidebar
-            // Skip the base lists
-           if (list.id === 'Inbox' || list.id === 'Today' || list.id === 'Upcoming') {
-               return
-           }
-            const listElement = document.createElement('li')
-            listElement.classList.add('list-name')
             
-            const iconElement = document.createElement("i");
-            iconElement.className = "fa-regular fa-folder";
-            iconElement.style.color = "#0061ff";
-            listElement.appendChild(iconElement)
-            
-            const textNode = document.createTextNode(list.title)
-            listElement.appendChild(textNode)
-            
-            listsList.appendChild(listElement)
         });
-
-        
     }
     
-    // Create new list
-    addListForm.addEventListener('submit', e => {
-        e.preventDefault()
-        const listTitle = addListFormInput.value
-        const newList = new TodoManager.List(listTitle)
-        addListFormInput.value = null
-        lists.push(newList)
-        render()
-        rotateBtn(addListBtnDisp);
-        toggleForm(addListForm)
+    
+    function clickHandlerBoard() {
+        addListBtnDisp.addEventListener('click', function() {
+            rotateBtn(addListBtnDisp);
+            toggleForm(addListForm);
+        });
+        
+        addTaskBtnDisp.addEventListener('click', function() {
+            rotateBtn(addTaskBtnDisp);
+            toggleForm(addTaskForm);
+        });
+    
+        // Create new list
+        addListForm.addEventListener('submit', e => {
+            e.preventDefault()
+            const listTitle = addListFormInput.value
+            const newList = new TodoManager.List(listTitle)
+            addListFormInput.value = null
+            lists.push(newList)
+            render()
+            rotateBtn(addListBtnDisp);
+            toggleForm(addListForm)
+            clickHandlerBoard()
+         })
+        
+         
+         //  Switch lists
+        let listTitles = document.querySelectorAll('li.list-name');
+        listTitles.forEach(list => {
+            list.addEventListener('click', function() {
+            // remove higlights on the sidebar
+            listTitles.forEach(title => {
+                title.classList.remove('selected')
+        })
+                let selectedTitleId = list.dataset.listId
+                TodoManager.selectList(selectedTitleId);
+                render();
+                clickHandlerBoard()
+            })
+        })
 
-    })
-
+    }
     render();
+    clickHandlerBoard();
 };
 
-ScreenController();
+ScreenController()
